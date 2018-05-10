9a7d88dce573fef82fb06827918be234a507d207
Tan Nguyen <dtsdaa@mgail.com>
2018-05-10 04:02:39 -0400

    cleaned up some code

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,17 +2,11 @@ import React from 'react';
 import Message from './Message';
 import PropTypes from 'prop-types';
 
-const Timeline = ({messages}) => {
-    return (
-        <div>
-            {messages.map(({id, ...props}) => {
-                return (
-                    <Message key={id} id={id} {...props} />
-                );
-            })}
-        </div>
-    );
-};
+const Timeline = ({messages}) => (
+    <div>
+        {messages.map(({id, ...props}) => ( <Message key={id} id={id} {...props} /> ))}
+    </div>
+);
 
 Timeline.propTypes = {
     messages: PropTypes.array
