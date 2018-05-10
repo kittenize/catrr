9c0ad2df17368db00055d4c161b47209c4a8c432
Tan Nguyen <dtsdaa@mgail.com>
2018-05-10 06:17:46 -0400

    cleaned up

--- src/containers/ApolloMessageInputContainer.js
+++ src/containers/ApolloMessageInputContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import gql from "graphql-tag";
-import { Mutation } from "react-apollo";
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 
 import MessageInput from '../components/MessageInput';
 
